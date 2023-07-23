699cf170317d9e36e97f731191e15779ca631f79
roadlittledawn <clinton.langosch@gmail.com>
2023-07-22 20:19:05 -0700

    feat: add input for output dir

--- create-new-article.sh
+++ create-new-article.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 templates_dir="templates"
-output_dir="output"
 
 # Check if the templates directory exists
 if [ ! -d "$templates_dir" ]; then
     echo "Error: The 'templates' directory does not exist. Please create the 'templates' directory and place your templates there."
     exit 1
-fi
-
-# Check if the output directory exists, if not, create it
-if [ ! -d "$output_dir" ]; then
-    mkdir "$output_dir"
 fi
 
 # Prompt user for the template type
@@ -26,13 +20,29 @@ if [ ! -f "$templates_dir/$template_name" ]; then
     exit 1
 fi
 
+# Prompt user for the output directory
+echo -n "Enter the name of the output directory (press Enter for default 'output' directory): "
+read custom_output_dir
+
+# Set the output directory to the default 'output' directory if not provided
+if [ -z "$custom_output_dir" ]; then
+    output_dir="output"
+else
+    output_dir="$custom_output_dir"
+fi
+
+# Check if the output directory exists, if not, create it
+if [ ! -d "$output_dir" ]; then
+    mkdir "$output_dir"
+fi
+
 # Prompt user for the number of files to create
 echo -n "Enter the number of files you want to create: "
 read num_files
 
 # Create the files from the selected template
 for ((i = 1; i <= num_files; i++)); do
-    cp "$templates_dir/$template_name" "$output_dir/${template_name}_file$i"
+    cp "$templates_dir/$template_name" "$output_dir/${template_name}"
 done
 
-echo "Files created successfully in the 'output' directory."
+echo "Files created successfully in the '$output_dir' directory."
